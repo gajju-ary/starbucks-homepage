9faeb5921d286f0a9647bb24a7f5e1623ed4f6ee
gajju-ary <gajendrachoudhary18@gmail.com>
2023-02-13 08:29:38 +0530

    initial commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function ImgSlider(anything){
+    document.querySelector('.Starbucks').src = anything;
+}
+function CircleColorChange(color){
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;  
+}
